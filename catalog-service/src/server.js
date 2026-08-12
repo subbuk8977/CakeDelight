fc1f11c441086a5cde21bd530f35dc860d44329e
subbuk8977 <chinnodu0987@gmail.com>
2026-08-12 09:17:09 +0530

    updated catalog-service

--- catalog-service/src/server.js
+++ catalog-service/src/server.js
@@ -6,25 +6,8 @@ const connectDB = require("./config/dbConnect");
 const express = require('express');
 const PORT = process.env.PORT || 4001;
 
+connectDB();
+
 const server = app.listen(PORT, () => {
   console.log(`catalog-service listening on port ${PORT}`);
-  connectDB();
-});
-
-process.on("SIGTERM", () => {
-  console.log("[catalog-service] SIGTERM received, shutting down");
-
-  server.close(() => {
-    console.log("[catalog-service] HTTP server closed");
-    process.exit(0);
-  });
-});
-
-process.on("SIGINT", () => {
-  console.log("[catalog-service] SIGINT received, shutting down");
-
-  server.close(() => {
-    console.log("[catalog-service] HTTP server closed");
-    process.exit(0);
-  });
 });
